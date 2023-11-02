ae36ed4cdf7b2f7c1af22c2466d60c5325bfdbfe
Lietziboy <michael.lietz@gmx.at>
2023-11-02 13:13:55 +0100

    Added refined_iron as an item

--- src/main/java/joch2em/projectgunsmithing/ProjectGunSmithing.java
+++ src/main/java/joch2em/projectgunsmithing/ProjectGunSmithing.java
@@ -1,7 +1,10 @@
 package joch2em.projectgunsmithing;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +14,16 @@ public class ProjectGunSmithing implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("projectgunsmithing");
 
+	public static final Item modItem = new Items(new Item.Settings().group(ItemGroup.MISC));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+
+		// registering a item called refined_iron with the id test.refined_iron
+		Registry.register(Registry.ITEM, new Identifier("gunsmithing", "refined_iron"), modItem);
+		LOGGER.info("Registered item!");
 	}
 }
