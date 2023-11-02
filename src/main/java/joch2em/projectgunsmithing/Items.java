4d757950d76678846f56b8c86bede73b3b826b05
Lietziboy <michael.lietz@gmx.at>
2023-11-02 13:13:39 +0100

    Added the items class

---
+++ src/main/java/joch2em/projectgunsmithing/Items.java
@@ -0,0 +1,10 @@
+package joch2em.projectgunsmithing;
+
+import net.minecraft.item.Item;
+
+public class Items extends Item {
+    public Items(Settings settings) {
+        super(settings);
+    }
+}
+
