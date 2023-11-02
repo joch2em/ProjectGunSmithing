430294e1705d3def2eb160691b0d46f6b3fdd2e6
joch3em <57416031+joch2em@users.noreply.github.com>
2023-11-02 12:14:01 +0100

    Initial commit

---
+++ src/client/java/joch2em/projectgunsmithing/ProjectGunSmithingClient.java
@@ -0,0 +1,10 @@
+package joch2em.projectgunsmithing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectGunSmithingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/joch2em/projectgunsmithing/ProjectGunSmithingDataGenerator.java
@@ -0,0 +1,11 @@
+package joch2em.projectgunsmithing;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ProjectGunSmithingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
